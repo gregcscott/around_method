2df8b2846d382ea9e9092db6c67567cb1d411b9f
gscott <gregory.c.scott@gmail.com>
2018-01-01 14:01:37 +0000

    Allow around_method to be declared anywhere in the class.

--- lib/around_method/core.rb
+++ lib/around_method/core.rb
@@ -1,11 +1,46 @@
 module AroundMethod
   module Core
     def around_method(method_name, wrapper_method)
+      apply_around_method(method_name, wrapper_method) if method_defined?(method_name)
+      around_method_wrapped[method_name] = wrapper_method
+    end
+
+    def apply_around_method(method_name, wrapper_method)
       renamed_original_method = "#{method_name}_inner"
       alias_method(renamed_original_method, method_name)
-      define_method(method_name) do
-        method(wrapper_method).call(&method(renamed_original_method))
+      with_method_added_hook_disabled do
+        define_method(method_name) do
+          original_method_proc = method(renamed_original_method)
+          method(wrapper_method).call(&original_method_proc)
+        end
       end
+    end
+
+    def method_added(method_name)
+      if around_method_wrapped.has_key?(method_name) && enable_method_added_hook?
+        apply_around_method(method_name, around_method_wrapped[method_name])
+      end
+      super
+    end
+
+    def around_method_wrapped
+      @around_method_wrapped ||= {}
+    end
+
+    def disable_method_added_hook?
+      @disable_method_added_hook
+    end
+
+    def enable_method_added_hook?
+      !disable_method_added_hook?
+    end
+
+    # Allow AroundMethod's method_added hook to be temporarily disabled,
+    # otherwise application will trigger an infinite loop.
+    def with_method_added_hook_disabled
+      @disable_method_added_hook = true
+      yield
+      @disable_method_added_hook = false
     end
   end
 end
--- spec/around_method_spec.rb
+++ spec/around_method_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe AroundMethod do
+  let(:instance) { klass.new }
+
   it 'has a version number' do
     expect(AroundMethod::VERSION).not_to be nil
   end
 
-  context 'when included into a class' do
+  shared_examples_for "method is wrapped" do
+    it 'wraps methods' do
+      expect(instance.wrapped).to eq 'cognito ergo sum'
+    end
+  end
+
+  context 'when around_method is called after method is defined' do
     let(:klass) do
       Class.new do
         def wrapped
@@ -16,13 +24,49 @@ RSpec.describe AroundMethod do
           yield + ' ergo sum'
         end
 
-        around_method(:wrapped, :wrapper)
+        around_method :wrapped, :wrapper
       end
     end
-    let(:instance) { klass.new }
 
-    it 'wraps methods' do
-      expect(instance.wrapped).to eq 'cognito ergo sum'
+    it_behaves_like "method is wrapped"
+  end
+
+  context 'when around_method is called before method is defined' do
+    let(:klass) do
+      Class.new do
+        around_method :wrapped, :wrapper
+
+        def wrapped
+          'cognito'
+        end
+
+        def wrapper
+          yield + ' ergo sum'
+        end
+      end
     end
+
+    it_behaves_like "method is wrapped"
+  end
+
+  context 'when around_method is called when method is defined on a superclass' do
+    let(:klass) do
+      Class.new(superclass) do
+        around_method :wrapped, :wrapper
+
+        def wrapper
+          yield + ' ergo sum'
+        end
+      end
+    end
+    let(:superclass) do
+      Class.new do
+        def wrapped
+          'cognito'
+        end
+      end
+    end
+
+    it_behaves_like "method is wrapped"
   end
 end
